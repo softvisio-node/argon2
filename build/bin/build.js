b762f2985136f9302ec2613433addd4faa2fff4a
zdm <zdm@softvisio.net>
2022-01-03 19:50:47 +0200

    feat: bindings

--- build/bin/build.js
+++ build/bin/build.js
@@ -4,7 +4,6 @@ import "#core/stream";
 import { resolve } from "#core/utils";
 import path from "path";
 import GitHubApi from "#core/api/github";
-import glob from "#core/glob";
 import File from "#core/file";
 import fs from "fs";
 import zlib from "zlib";
@@ -20,10 +19,9 @@ const gitHubApi = new GitHubApi( process.env.GITHUB_TOKEN );
 const release = await gitHubApi.getReleaseByTagName( REPO, TAG );
 if ( !release.ok ) process.exit( 1 );
 
-for ( const file of glob( "*.node", { cwd, "sync": true } ) ) {
-    const res = await gitHubApi.updateReleaseAsset( REPO, release.data.id, await repack( path.join( cwd, file ) ) );
-    if ( !res.ok ) process.exit( 1 );
-}
+const res = await gitHubApi.updateReleaseAsset( REPO, release.data.id, await repack( path.join( cwd, "lib/binding/napi-v3/argon2.node" ) ) );
+
+if ( !res.ok ) process.exit( 1 );
 
 async function repack ( _path ) {
     const name = `napi-v3-${process.platform}-${process.arch}.node.gz`;
