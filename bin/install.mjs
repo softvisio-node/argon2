d780d94c10788580aa5b843578af96a3cf55a233
zdm <zdm@softvisio.net>
2023-05-20 16:06:26 +0300

    fix: update script

--- bin/install.mjs
+++ bin/install.mjs
@@ -1,37 +1,41 @@
 #!/usr/bin/env node
 
-import https from "https";
-import zlib from "zlib";
-import fs from "fs";
+import fs from "node:fs";
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import zlib from "node:zlib";
 
 const url = new URL( "https://github.com/softvisio-node/argon2/releases/download/data/" );
 
 await get( url, `napi-v3-${process.platform}-${process.arch}.node` );
 
 // under windows download linux binaries for vmware
-if ( process.platform === "win32" ) await get( url, `napi-v3-linux-${process.arch}.node` );
-
-process.exit();
+if ( process.platform === "win32" ) {
+    await get( url, `napi-v3-linux-${process.arch}.node` );
+}
 
 async function get ( url, file ) {
     process.stdout.write( `Downloading: ${file} ... ` );
 
-    const res = await new Promise( resolve => {
-        https.get( url + file + ".gz", res => {
-            if ( res.statusCode !== 302 ) return resolve();
+    const res = await fetch( url + file + ".gz" );
 
-            https.get( res.headers.location, res => {
-                fs.mkdirSync( "lib/binaries", { "recursive": true } );
+    if ( !res.ok ) {
+        console.log( res.status + " " + res.statusText );
 
-                res.pipe( zlib.createGunzip() )
-                    .pipe( fs.createWriteStream( `lib/binaries/${file}` ) )
-                    .on( "close", () => resolve( true ) )
-                    .on( "error", e => resolve() );
-            } );
-        } );
-    } );
+        process.exit( 1 );
+    }
+    else {
+        fs.mkdirSync( "lib/binaries", { "recursive": true } );
 
-    console.log( res ? "OK" : "FAIL" );
+        const e = await pipeline( Readable.fromWeb( res.body ), zlib.createGunzip(), fs.createWriteStream( `lib/binaries/${file}` ) ).catch( e => e );
 
-    if ( !res ) process.exit( 1 );
+        if ( e ) {
+            console.log( e + "" );
+
+            process.exit( 1 );
+        }
+        else {
+            console.log( "OK" );
+        }
+    }
 }
