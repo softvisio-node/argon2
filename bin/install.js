f0d7e2f6079e1875fe7adb53a2472fd63cbd23ed
zdm <zdm@softvisio.net>
2023-12-02 18:25:19 +0200

    fix: external resource

--- bin/install.js
+++ bin/install.js
@@ -2,11 +2,16 @@
 
 import externalResources from "@softvisio/utils/external-resources";
 
-externalResources.add( "softvisio-node/argon2/resources", { "napi": 3 } );
+externalResources.add( "softvisio-node/argon2/resources", {
+    "napi": 3,
+} );
 
 // under windows download linux binaries for vmware
 if ( process.platform === "win32" ) {
-    externalResources.add( "softvisio-node/argon2/resources", { "napi": 3, "platform": "linux" } );
+    externalResources.add( "softvisio-node/argon2/resources", {
+        "napi": 3,
+        "platform": "linux",
+    } );
 }
 
 const res = await externalResources.update( {
